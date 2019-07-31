cd12fd2d7416ef9cd4e1aa169da1b9ba2c6b98bd
AyushJadhav <37294348+AyushJadhav@users.noreply.github.com>
2019-07-31 19:24:20 +0530

    Update size.cpp

--- size.cpp
+++ size.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 int main(){
 	cout<<"Size of int-->"<<sizeof(int);
+	cout<<"\nSize of short-->"<<sizeof(short);
+	cout<<"\nSize of char-->"<<sizeof(char);
+	cout<<"\nSize of long-->"<<sizeof(long);
+	cout<<"\nSize of double-->"<<sizeof(double);
+	cout<<"\nSize of float-->"<<sizeof(float);
+	cout<<"\nSize of long double-->"<<sizeof(long double);
+	cout<<"\nSize of bool-->"<<sizeof(bool);
 
 }
