3f8bc7a13a298218dd79d7b7f2ef6f434b9eae1d
AyushJadhav <37294348+AyushJadhav@users.noreply.github.com>
2019-08-03 20:52:12 +0530

    Add files via upload

---
+++ size1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(){
+	cout<<"Size of int-->"<<sizeof(int);
+	cout<<"\nSize of short-->"<<sizeof(short);
+	cout<<"\nSize of char-->"<<sizeof(char);
+	cout<<"\nSize of long-->"<<sizeof(long);
+	cout<<"\nSize of double-->"<<sizeof(double);
+	cout<<"\nSize of float-->"<<sizeof(float);
+	cout<<"\nSize of long double-->"<<sizeof(long double);
+	cout<<"\nSize of bool-->"<<sizeof(bool);
+
+}
